97857c6169da068488885e8028a980b9adf329b9
Matt Sanders <matt@modal.org>
2012-04-10 12:08:43 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NotificationsSampleApp::Application.config.secret_token = '2ebaf101c2a5a904a7049ed971add2a054eab12c50c405328adcafc137d392a5e48afd80212092c8261e6f614957a607a8f1a187e62d68956895ab40e3dab8ca'
